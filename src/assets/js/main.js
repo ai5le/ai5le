fb7738cae4dc3b814e7a4fe90756265a862c81bc
Tommy Garceau <tommy@garceau.io>
2018-02-20 20:47:01 -0800

    updates

---
+++ src/assets/js/main.js
@@ -0,0 +1,10 @@
+// Add Background to Nav when scrolling
+//$(document).scroll(function() {
+//    if ($(this).scrollTop() > 260) {
+//        $('.top-bar').addClass('light');
+//    } else {
+//        $('.top-bar').removeClass('light');
+//    }
+//});
+
+// Add Open and Close to Menu Toggle 
