bacad20970293c55e41af0205e77adc2055844f9
Tommy Garceau <tommy@garceau.io>
2018-02-21 14:38:22 -0800

    updates

---
+++ dist/index.php
@@ -0,0 +1,154 @@
+<?php $home = true; ?>
+<?php include 'partials/header.php'; ?>
+
+
+    <!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="google-site-verification" content="Da65aO0SkW7VgxJlbxxVDyCRCkLqsp1iawpoZe4V3zI" />
+<title>All things web &amp; mobile app development &middot; AI5LE</title>
+<link rel="icon" href="assets/img/icons/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body>
+<!-- google analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112068380-1"></script>
+<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-112068380-1');</script><!-- off-canvas-wrapper -->
+<div class="off-canvas-wrapper">
+  <!-- off-canvas -->
+  <div class="off-canvas position-right" id="offCanvasRight" data-off-canvas>
+  
+      <ul class="menu vertical" data-accordion-menu>
+          <li>
+              <a href="">Home</a>
+          </li>
+          <li class="menu-text">What We Offer
+              </li>
+              <li>
+                  <a href="services">Our Services</a>
+                  <ul class="menu vertical submenu nested">
+                      <li>
+                          <a href="services/create.html">We Create</a>
+                      </li>
+                      <li>
+                          <a href="services/design.html">We Design</a>
+                      </li>
+                      <li>
+                          <a href="services/develop.html">We Develop</a>
+                      </li>
+                      <li>
+                          <a href="services/maintain.html">We Maintain</a>
+                      </li>
+                  </ul>
+              </li>
+  <li class="menu-text">What We've Done
+          </li>
+          <li>
+              <a href="projects/">Our Projects</a>
+              <ul class="menu vertical submenu nested">
+                  <li>
+                      <a href="projects/current.html">Current</a>
+                  </li>
+                  <li>
+                      <a href="projects/past.html">Past</a>
+                  </li>
+              </ul>
+          </li>            
+          <li class="menu-text">How It's Done
+          </li>
+          <li>
+              <a href="process/">Our Process</a>
+              <ul class="menu vertical submenu nested">
+                  <li>
+                      <a href="process/discovery.html">Discovery</a>
+                  </li>
+                  <li>
+                      <a href="process/design.html">Design</a>
+                  </li>
+                  <li>
+                      <a href="process/develop.html">Develop</a>
+                  </li>
+                  <li>
+                      <a href="process/debug.html">Debug</a>
+                  </li>
+                  <li>
+                      <a href="process/deploy.html">Deploy</a>
+                  </li>
+              </ul>
+          </li>
+          <li class="menu-text">When You're Ready
+          </li>
+          <li>
+              <a href="contact/">Contact Us</a>
+              <ul class="menu vertical submenu nested">
+                  <li>
+                      <a href="contact/">Our Info</a>
+                  </li>
+                  <li>
+                      <a href="contact/quote.html">Quote</a>
+                  </li>
+              </ul>
+          </li>
+          <li class="menu-text">Our Notebook</li>
+          <li><a href="notebook/">Notebook</a></li>
+      </ul>
+  
+  </div>  <!-- off-canvas-content-->
+  <div class="off-canvas-content" data-off-canvas-content>
+    <!-- header_home -->
+    <nav class="top-bar">
+            <div class="column large-4 medium-6 small-12">
+                <a href=""><img id="logo" src="assets/svg/ai5le-logo-sm.svg" alt="AI5LE" title="AI5LE &middot; Developing All Things Web, Mobile, and Desktop" /></a>
+            </div>
+            <div class="column ">
+                <ul class="menu">
+                    <li>
+                        <a data-toggle="offCanvasRight" id="menu-toggle"></a>
+                    </li>
+                </ul>
+            </div>
+    </nav>    <!-- page_content -->
+    
+    <section id="intro">
+        <div class="row large-up-3 medium-up-1 small-up-1" data-equilizer data-equalize-on="large">
+            <!-- create -->
+            <div class="intro column">
+                <div id="intro_create" data-equalizer-watch>
+                    <img class="intro_icon" src="assets/img/create.png" alt="create icon">
+                    <h3 class="intro_title">creates</h3>
+                    <h4 class="intro_subtitle">visual & graphic design user experiences</h4>
+                    <p class="intro_description">We create the experience and presence that will get your project noticed in print and online. You need more
+                        than just a site, you need traffic.</p>
+                    <a href="contact/" class="button small">let us create your next project →</a>
+                </div>
+            </div>
+            <!-- design -->
+            <div class="intro column">
+                <div id="intro_design" data-equalizer-watch>
+                    <img class="intro_icon" src="assets/svg/design.svg" alt="design icon">
+                    <h3 class="intro_title">designs</h3>
+                    <h4 class="intro_subtitle">mobile application and communication flows</h4>
+                    <p class="intro_description">We design the user interface and the work flow of communication services using the latest, including beta, telecommunication technology.</p>
+                    <a href="contact/" class="button small">let us design your next project →</a>
+                </div>
+            </div>
+            <!-- develop -->
+            <div class="intro column">
+                <div id="intro_develop" data-equalizer-watch>
+                    <img class="intro_icon" src="assets/svg/develop.svg" alt="develop icon">
+                    <h3 class="intro_title">develops</h3>
+                    <h4 class="intro_subtitle">all things web, mobile & desktop applicatons</h4>
+                    <p class="intro_description">We develop cross platform desktop applications that can be used on windows, mac, or linux. Let's make that
+                        app you've always wanted.</p>
+                    <a href="contact/" class="button small">let us develop your next project →</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- / .intro -->
+
+
+<?php include 'partials/footer.php'; ?>
---
+++ dist/partials/footer.php
@@ -0,0 +1,51 @@
+<!-- footer -->
+<footer class="row" id="ft-cols">
+    <!-- ft_about -->
+    <div class="column large-4 medium-12 small-12" id="ft_about">
+        <div class="ft_logo">
+            <img src="assets/svg/ai5le-logo-sm.svg" itemprop="image" alt="AI5LE Brand Image Logo" />
+        </div>
+
+        At <a href="">AI5LE</a>, we <a href="services/create.html">create</a> visual &amp; graphic design user experiences,  <a href="services/design.html">design</a> mobile application and communication flows, and <a href="services/develop.html">develop</a> all things web, mobile, and desktop.</p>    </div>
+    <!-- fixme -->
+    <div class="column large-4 medium-12 small-12 text-center">
+        &nbsp;
+    </div>
+    <!-- ft_quote -->
+    <div class="column large-4 medium-12 small-12 text-center" id="ft_quote">
+        <q>Simplicity is the ultimate sophistication.</q><br/> - Leonardo da Vinci    </div>
+
+</footer>
+
+<footer id="ft-bttm">
+    <div class="row">
+        <div class="column large-4 medium-12 small-12" id="ft-copy">
+            &copy; 2017-2018 <a href="" title="AI5LE - All Things Web and Mobile Development">AI5LE</a>.<br/> All rights reserved.
+        </div>
+        <div class="column large-4 medium-12 small-12 text-center" id="ft-cred">
+            <img src="assets/svg/code.svg" alt="Coded" title="Coded">
+            with
+            <img src="assets/svg/heart.svg" alt="Love" title="Love">
+            and
+            <img src="assets/svg/coffee.svg" alt="Coffee" title="Coffee">
+            <br/> for a better web.        </div>
+        <div class="column large-4 medium-12 small-12 text-right" id="ft-tel">
+            <div itemscope itemtype="http://schema.org/LocalBusiness">
+                CALL:
+                <span itemprop="telephone">
+                        <a href="tel:+13236882001">
+                            323-688-2001</a>
+                    </span>
+                <br/>EMAIL:
+                <a href="mailto:hello@ai5le.com" itemprop="email">
+                    hello@ai5le.com</a>
+            </div>
+
+        </div>
+    </div>
+</footer>  </div><!-- end off-canvas-content -->
+</div><!-- end off-canvas-wrapper -->
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
